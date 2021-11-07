1034edaed376082809986aabffd73bfbdd904227
Teszáry Péter <peterteszary@gmail.com>
2021-11-07 08:10:34 +0100

    Update info

---
+++ study-wp-plugin-development.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * * @package study-wp-plugin-development
+ */
+/*
+Plugin Name: study-wp-plugin-development
+Plugin URI: https://peterteszary.com
+Description: This is my Test Plugin for study purpose.
+Version 1.0.0
+Author: Peter Teszáry
+Author URI: https://peterteszary.com
+Lisence: GPLv2 or later
+Text Domain: study-wp-plugin-development
+*/
+
+
+
+?>
