df65ede461e1c26aa0fc47273af9600f83fa63e8
Teszáry Péter <peterteszary@gmail.com>
2021-11-16 21:41:32 +0100

    $wpdb->query( "DELETE FROM wp_posts WHERE post_type = 'book'" )

--- uninstall.php
+++ uninstall.php
@@ -13,6 +13,10 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 // Clear Database stored data
 $books = get_posts( array( 'post_type' => 'book', 'numberposts' => -1 ) );
 
-foreach( $books as $book) {
-    wp_delete_posts( $book->ID, true );
-}
+// foreach( $books as $book) {
+//    wp_delete_posts( $book->ID, true );
+// }
+
+// Access the database via SQL
+global $wpdb;
+$wpdb->query( "DELETE FROM wp_posts WHERE post_type = 'book'" )
