10a6dafb5a134e26713aab140045bfc49dbd0681
Teszáry Péter <peterteszary@gmail.com>
2021-11-16 21:44:09 +0100

    wpdb

--- uninstall.php
+++ uninstall.php
@@ -19,4 +19,5 @@ $books = get_posts( array( 'post_type' => 'book', 'numberposts' => -1 ) );
 
 // Access the database via SQL
 global $wpdb;
-$wpdb->query( "DELETE FROM wp_posts WHERE post_type = 'book'" )
+$wpdb->query( "DELETE FROM wp_posts WHERE post_type = 'book'" );
+$wpdb->query( "DELETE FROM wp_postmeta WHERE post_id NOT IN (SELECT id FROM wp_posts)" );
