25881712122e738db815e2a0ff0447531e6bdf63
Teszáry Péter <peterteszary@gmail.com>
2021-11-18 22:05:24 +0100

    if ( !class_exists( 'TestPlugin' ) ) {

--- study-wp-plugin-development.php
+++ study-wp-plugin-development.php
@@ -14,16 +14,32 @@ Text Domain: study-wp-plugin-development
 */
 
 
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+Copyright 2005-2015 Automattic, Inc.
+*/
+
+
  defined( 'ABSPATH' ) or die( 'Hey, you cannot access this file!' ); 
  
 
 /* the plugin itself */
-
+if ( !class_exists( 'TestPlugin' ) ) {
 
 class TestPlugin 
 {
 
-         public $plugin;
+        public $plugin;
 
         function __construct() {
                 $this->plugin = plugin_basename( __FILE__ );
@@ -84,3 +100,4 @@ class TestPlugin
                 // deactivation
                 require_once plugin_dir_path( __FILE__ ) . 'inc/test-plugin-deactivate.php';
                 register_activation_hook( __FILE__, array( 'TestPluginDeactivate', 'deactivate' ) );
+}
