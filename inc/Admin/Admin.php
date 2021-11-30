ca817cee7c65ccf6671c0d740e0d6e1142d4d4c8
Teszáry Péter <peterteszary@gmail.com>
2021-11-30 20:02:19 +0100

    fff

--- inc/Admin/Admin.php
+++ inc/Admin/Admin.php
@@ -5,9 +5,20 @@
 
 namespace Inc\Pages;
 
-class Admin{
+class Admin
+{  
+    public function register() {
 
-    function __construct() 
-    {}
+        add_action( 'admin_menu', array( $this, 'add_admin_pages') );
+
+    }
+
+    public function add_admin_pages() {
+        add_menu_page( 'TesTinator Plugin', 'Testinator', 'manage_options', 'test_plugin', array( $this, 'admin_index' ), 'dashicons-superhero', 110 );
+    }
+    public function admin_index() {
+        require_once PLUGIN_PATH . 'templates/admin.php';
+    }
+
 
 }
