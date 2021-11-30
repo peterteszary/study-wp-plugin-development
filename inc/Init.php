b826649a2da14ccc83f9fa8a5b560a81ed24ad00
Teszáry Péter <peterteszary@gmail.com>
2021-11-30 20:05:13 +0100

    )

--- inc/Init.php
+++ inc/Init.php
@@ -22,8 +22,9 @@ namespace Inc;
          * @returnd [type] [description]
          */
 
-        public static function register_services() {
-            foreach ( self::get_services() as $class  {
+        public static function register_services() 
+        {
+            foreach ( self::get_services() as $class)  {
                 $service = self::instantiate( $class );
                 if ( method_exists( $service, 'register') ) {
                     $service->register();
