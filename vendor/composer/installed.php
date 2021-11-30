1668288c6c8195d017f44241820f1dd73fd11e79
Teszáry Péter <peterteszary@gmail.com>
2021-11-30 20:17:32 +0100

    sss

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'a99d4570179adf2d79472f8b7ebcd425f3635dbd',
+    'name' => 'peterteszary/study-wp-plugin-development',
+  ),
+  'versions' => 
+  array (
+    'peterteszary/study-wp-plugin-development' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a99d4570179adf2d79472f8b7ebcd425f3635dbd',
+    ),
+  ),
+);
