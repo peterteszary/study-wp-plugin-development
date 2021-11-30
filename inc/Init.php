213457dfe18db3486baba60585186015bddc3b96
Teszáry Péter <peterteszary@gmail.com>
2021-11-30 20:02:04 +0100

    Plugin Path

--- inc/Init.php
+++ inc/Init.php
@@ -4,13 +4,46 @@
  */
 namespace Inc;
 
- class init
+ final  class Init
  {
-        function __construct {
-            
+
+    /*
+    * Store all the classes inside an array
+    * @return array Full list of classes
+    */
+     public static function get_services() {
+         return [
+             Pages\Admin::class
+         ];
+     }
+
+        /**
+         * Loop through the classes, initialize them, and call the register() method if it exists
+         * @returnd [type] [description]
+         */
+
+        public static function register_services() {
+            foreach ( self::get_services() as $class  {
+                $service = self::instantiate( $class );
+                if ( method_exists( $service, 'register') ) {
+                    $service->register();
+                }
+            })
         }
 
-        
+        /**
+         * 
+         * Initialize the class
+         * @param class $class class from the services array
+         * @return class instance new instance of the class
+         */
+
+       private static function instantiate( $class )
+       {
+           $service = new $class();
+
+           return $service
+       } 
 
  }
  
