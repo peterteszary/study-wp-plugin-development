eb5cf021c0c51a9a962847a8ac41d0286f828c45
Teszáry Péter <peterteszary@gmail.com>
2021-12-01 19:57:16 +0100

    sss

--- study-wp-plugin-development.php
+++ study-wp-plugin-development.php
@@ -42,6 +42,21 @@ define( 'PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 define( 'PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'PLUGIN', plugin_basename( __FILE__ ) );
 
+use Inc\Base\Activate;
+use Inc\Base\Deactivate;
+
+function activate_test_plugin() {
+	Activate::activate();
+}
+
+function deactivate_test_plugin() {
+	Deactivate::deactivate();
+}
+
+register_activation_hook( __FILE__, 'activate_test_plugin' )
+register_deactivation_hook( __FILE__, 'deactivate_test_plugin' )
+
+
 if ( class_exists( 'Inc\\Init' ) ) {
 	Inc\Init::register_services();
 }
