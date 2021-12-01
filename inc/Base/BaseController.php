4d93eb0395e4538ea7438cad2bb0445ae40e38d4
Teszáry Péter <peterteszary@gmail.com>
2021-12-01 22:20:16 +0100

    www

--- inc/Base/BaseController.php
+++ inc/Base/BaseController.php
@@ -8,7 +8,13 @@ class BaseController
 {
 	public $plugin_path;
 
-    public function __construct() {
-        $this->plugin_path = plugin_dir_path( dirname( __FILE__, 2 ) );
-    }
+	public $plugin_url;
+
+	public $plugin;
+
+	public function __construct() {
+		$this->plugin_path = plugin_dir_path( dirname( __FILE__, 2 ) );
+		$this->plugin_url = plugin_dir_url( dirname( __FILE__, 2 ) );
+		$this->plugin = plugin_basename( dirname( __FILE__, 3 ) ) . '/study-wp-plugin-development.php';
+	}
 }
