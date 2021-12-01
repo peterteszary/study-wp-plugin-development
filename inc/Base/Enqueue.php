c42afe39015c668e2321375f041714cbe9eec80c
Teszáry Péter <peterteszary@gmail.com>
2021-12-01 22:30:48 +0100

    sss

--- inc/Base/Enqueue.php
+++ inc/Base/Enqueue.php
@@ -7,18 +7,17 @@ namespace Inc\Base;
 
 use \Inc\Base\BaseController;
 
-/**
-* 
-*/
-class Enqueue extends BaseController
+class SettingsLinks extends BaseController
 {
-	public function register() {
-		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue' ) );
+	public function register() 
+	{
+		add_filter( "plugin_action_links_$this->plugin", array( $this, 'settings_link' ) );
 	}
-	
-	function enqueue() {
-		// enqueue all our scripts
-		wp_enqueue_style( 'mypluginstyle', $this->plugin_url . 'assets/mystyle.css' );
-		wp_enqueue_script( 'mypluginscript', $this->plugin_url . 'assets/myscript.js' );
+
+	public function settings_link( $links ) 
+	{
+		$settings_link = '<a href="admin.php?page=test_plugin">Settings</a>';
+		array_push( $links, $settings_link );
+		return $links;
 	}
 }
