60a7e69d84b7182fd0b901318736c5faca94b71d
Teszáry Péter <peterteszary@gmail.com>
2021-12-01 22:23:04 +0100

    111

--- inc/Base/SettingsLinks.php
+++ inc/Base/SettingsLinks.php
@@ -4,23 +4,19 @@
  */
 namespace Inc\Base;
 
-class SettingsLinks
+use \Inc\Base\BaseController;
+
+class SettingsLinks extends BaseController
 {
-protected $plugin;
+	public function register() 
+	{
+		add_filter( "plugin_action_links_$this->plugin", array( $this, 'settings_link' ) );
+	}
 
-public function __construct() 
-{
-    $this->plugin = PLUGIN;
-}
-
-    public function register() {
-        add_filter( "plugin_action_links_$this->plugin", array( $this, 'settings_link') );
-
-    }
-
-	public function settings_link ( $links ) {
-        $settings_link = '<a href="admin.php?page=test_plugin">Settings</a>';
-        array_push( $links, $settings_link);
-        return $links;
+	public function settings_link( $links ) 
+	{
+		$settings_link = '<a href="admin.php?page=test_plugin">Settings</a>';
+		array_push( $links, $settings_link );
+		return $links;
 	}
 }
