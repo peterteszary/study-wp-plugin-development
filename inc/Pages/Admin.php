5b3ab7e7ad1cf65bd009b323fedcbac1888e58f5
Teszáry Péter <peterteszary@gmail.com>
2021-12-02 21:02:18 +0100

    ssss

--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -23,34 +23,19 @@ class Admin extends BaseController
 
 		$this->pages = array(
 			array(
-				'page_title' => 'Testinator Plugin', 
+				'page_title' => 'Testinator', 
 				'menu_title' => 'Testinator', 
 				'capability' => 'manage_options', 
 				'menu_slug' => 'test_plugin', 
 				'callback' => function() { echo '<h1>Testinator Plugin</h1>'; }, 
 				'icon_url' => 'dashicons-superhero', 
 				'position' => 110
-                )
-            );
-        }
-    
-        public function register() 
-        {
-            $this->settings->addPages( $this->pages )->register();
-        }
-    }
-/*
-    public function register() {
-        add_action( 'admin_menu', array( $this, 'add_admin_pages') );
-    }
+			)
+		);
+	}
 
-    public function add_admin_pages() {
-        add_menu_page( 'TesTinator Plugin', 'Testinator', 'manage_options', 'test_plugin', array( $this, 'admin_index' ), 'dashicons-superhero', 110 );
-    }
-    
-    public function admin_index() {
-		require_once $this->plugin_path . 'templates/admin.php';
-    }
-
-
+	public function register() 
+	{
+		$this->settings->addPages( $this->pages )->register();
+	}
 }
