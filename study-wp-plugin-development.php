d441090ec84c483df2958c8557b435b9cb7f0844
Teszáry Péter <peterteszary@gmail.com>
2021-12-02 20:46:00 +0100

    sss

--- study-wp-plugin-development.php
+++ study-wp-plugin-development.php
@@ -6,7 +6,7 @@
 Plugin Name: study-wp-plugin-development
 Plugin URI: https://github.com/peterteszary/study-wp-plugin-development.git
 Description: This is my Test Plugin for study purpose.
-Version 1.0.0
+Version: 1.0.0
 Author: Peter Teszáry
 Author URI: https://peterteszary.com
 Lisence: GPLv2 or later
