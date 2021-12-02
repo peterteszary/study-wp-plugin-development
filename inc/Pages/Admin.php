d2a5997e90fc4dd41b5e57a7f3bf9f38b07aa7fc
Teszáry Péter <peterteszary@gmail.com>
2021-12-02 20:45:38 +0100

    SettingsApi.php

--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -6,9 +6,38 @@
 namespace Inc\Pages;
 
 use \Inc\Base\BaseController;
+use \Inc\Api\SettingsApi;
 
 class Admin extends BaseController
 {  
+
+    public $settings;
+
+	public $pages = array();
+
+	public function __construct()
+	{
+		$this->settings = new SettingsApi();
+
+		$this->pages = array(
+			array(
+				'page_title' => 'Testinator Plugin', 
+				'menu_title' => 'Testinator', 
+				'capability' => 'manage_options', 
+				'menu_slug' => 'tert_plugin', 
+				'callback' => function() { echo '<h1>Testinator Plugin</h1>'; }, 
+				'icon_url' => 'dashicons-superhero', 
+				'position' => 110
+			)
+		);
+	}
+
+    public function register() 
+	{
+		$this->settings->addPages( $this->pages )->register();
+	}
+}
+/*
     public function register() {
         add_action( 'admin_menu', array( $this, 'add_admin_pages') );
     }
